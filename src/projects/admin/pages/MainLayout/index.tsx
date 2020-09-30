2472ff09176df123fbdfe95a8fca867070fbd78f
qingyanglove33 <chenliangtomato@gmail.com>
2020-09-30 17:50:48 +0800

    [u]修改了menu

--- src/projects/admin/pages/MainLayout/index.tsx
+++ src/projects/admin/pages/MainLayout/index.tsx
@@ -1,24 +1,22 @@
 import * as React from 'react';
 import { Layout, message, Button,Menu, Breadcrumb } from 'antd';
 import { Link } from 'react-router-dom';
-import { renderRoutes } from 'react-router-config';
+// import { renderRoutes } from 'react-router-config';
+import { renderRoutes } from '@/projects/admin/router/router-config';
+
 import { connect } from "react-redux";
 import { getUserAction, loginOut } from '@/projects/admin/store/action'
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
 import logo from "@/assets/images/logo.svg";
 import SiderMenu from './SiderMenu'
 const styles = require('./index.module.less');
 
-const { SubMenu } = Menu;
 const { Header, Footer, Sider, Content } = Layout;
-
 export interface IAppProps {
 
 }
 
 export interface IAppState {
-  openKeys: string,
-  selectedKeys: string
+  collapsed: boolean
 }
 
 const mapStateToProps = (state: any) => {
@@ -36,19 +34,21 @@ const mapDispatchToProps = (dispatch: any) => ({
       dispatch(loginOut());
   },
   
-});
+})
+const breadcrumbNameMap = {
+  '/admin': 'subna1',
+  '/admin/index': '首页',
+  '/admin/order': '订单',
+  '/admin/finance': '财务',
+  '/admin/finance/detail': 'Detail',
+}
 class App extends React.Component<any, IAppState> {
   constructor(props: IAppProps) {
     super(props);
 
     this.state = {
-      openKeys:'admin',
-      selectedKeys: 'order'
+      collapsed: false
     }
-  }
-
-  componentDidUpdate(props: any) {
-    console.log(props.history);    
   }
   componentDidMount() {
     this.props.requestUser();
@@ -56,40 +56,45 @@ class App extends React.Component<any, IAppState> {
   handleClick = () => {
     this.props.loginOut();
  }
- menuClick = (res:any) => {
-   let { item, key, keyPath, domEvent } = res;
-   debugger
-   this.setState({
-      selectedKeys: key
-    })
-  // this.props.history.push(`/admin/${key}`)
- }
- onOpenChange = () => {
-
- }
+ onCollapse = (collapsed: boolean) => {
+  console.log(collapsed);
+  this.setState({ collapsed });
+}
   public render() {
+    const { location } = this.props;
+    const pathSnippets = location.pathname.split('/').filter((i: any) => i);
+    const extraBreadcrumbItems = pathSnippets.map((_: any, index: number) => {
+      const url:string = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+      return (
+        <Breadcrumb.Item key={url}>
+          {index === pathSnippets - 1? (breadcrumbNameMap as any)[url] : <Link to={url}>{(breadcrumbNameMap as any)[url]}</Link>}
+        </Breadcrumb.Item>
+      );
+    });
+    const breadcrumbItems = [
+      <Breadcrumb.Item key="home">
+        <Link to="/">Home</Link>
+      </Breadcrumb.Item>,
+    ].concat(extraBreadcrumbItems);
     const { routes } = this.props.route;
     return (
       <Layout>
-        <Header className="header">
+        <Header className={styles.header}>
         <img src={logo} className={styles['app--logo']} alt="logo" />
+        <Button className={styles.btn_logout} type="primary" onClick={() => this.props.loginOut()}>退出登录</Button>
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
             <Menu.Item key="1">首页</Menu.Item>
             <Menu.Item key="2">订单</Menu.Item>
             <Menu.Item key="3">财务</Menu.Item>
           </Menu>
-          <Button className={styles.btn_logout} type="primary" onClick={() => this.props.loginOut()}>退出登录</Button>
         </Header>
-       
-         <Layout className="site-layout-background">
-            <Sider className="site-layout-background" width={200}>
+         <Layout className={styles.container}>
+            <Sider className="site-layout-background" width={200} collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
                <SiderMenu  {...this.props}></SiderMenu>
             </Sider>
             <Layout style={{ padding: '0 24px 24px' }}>
-              <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item>List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
+              <Breadcrumb style={{ margin: '18px 0' }}>
+                {breadcrumbItems}
               </Breadcrumb>
               <Content
                 className="site-layout-background"
